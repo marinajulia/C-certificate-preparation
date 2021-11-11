4a0593038e362978702fc64ea7e572c5392fb342
marinajulia <marinajulia707@gmail.com>
2021-11-10 21:15:26 -0300

    resolução de sobrecargas

--- structs/2/Resolucao de Sobrecarga/ResolucaoDeSobrecarga.cs
+++ structs/2/Resolucao de Sobrecarga/ResolucaoDeSobrecarga.cs
@@ -1,4 +1,6 @@
-﻿namespace Programming._2.Resolucao_de_Sobrecarga
+﻿using System;
+
+namespace Programming._2.Resolucao_de_Sobrecarga
 {
     public class ResolucaoDeSobrecarga
     {
@@ -12,11 +14,27 @@
 
             double double1 = 123.45;
             double double2 = 456.78;
+            Console.WriteLine(Somar(int1, int2));
+            Console.WriteLine(Somar(short1, short2));
+            Console.WriteLine(Somar(double1, double2));
         }
 
-        int Somar(int parcela1, int parcela2)
+        //int Somar(int parcela1, int parcela2)
+        //{
+        //    return parcela1 + parcela2;
+        //}
+        //short Somar(short parcela1, short parcela2)
+        //{
+        //    return (short)(parcela1 + parcela2);
+        //}
+        //object Somar(object parcela1, object parcela2)
+        //{
+        //    return (double)parcela1 + (double)parcela2;
+        //}
+        dynamic Somar(dynamic parcela1, dynamic parcela2)
         {
             return parcela1 + parcela2;
         }
+
     }
 }
