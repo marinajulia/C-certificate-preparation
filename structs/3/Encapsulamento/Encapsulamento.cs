e578d220baa007481452d5db53b15d13b1cd627e
marinajulia <marinajulia707@gmail.com>
2021-11-13 17:37:52 -0300

    modificadores de visibilidade

--- structs/3/Encapsulamento/Encapsulamento.cs
+++ structs/3/Encapsulamento/Encapsulamento.cs
@@ -39,5 +39,40 @@ namespace Programming._3.Encapsulamento
 
         //auto implementada:
         //public decimal Salario { get; set; }
+
+    }
+    public class Banco
+    {
+        public Banco()
+        {
+            Conta conta = new Conta();
+            conta.Saldo = 1000;
+            Console.WriteLine(conta.Saldo);
+        }
+    }
+
+    public class Conta
+    {
+        public Conta()
+        {
+            this.Saldo = 1000;
+            Console.WriteLine(this.Saldo);
+        }
+
+        void Sacar(decimal saque)
+        {
+            Saldo = Saldo - saque;
+        }
+
+        public decimal Saldo { get; set; }
+    }
+
+    public class ContaCorrente : Conta
+    {
+        public ContaCorrente()
+        {
+            this.Saldo = 1000;
+            Console.WriteLine(this.Saldo);
+        }
     }
 }
