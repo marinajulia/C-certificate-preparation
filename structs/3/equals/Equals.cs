d8e0939001fe9d526d149108e278c0e43eaac812
marinajulia <marinajulia707@gmail.com>
2021-11-16 22:04:45 -0300

    IComparable

--- structs/3/equals/Equals.cs
+++ structs/3/equals/Equals.cs
@@ -26,10 +26,37 @@ namespace Programming._3.equals
 
             Console.WriteLine(aluno1.Equals(aluno2));
             Console.WriteLine(aluno1.Equals(aluno3));
+
+            Aluno aluno4 = new Aluno()
+            {
+                Nome = "andré",
+                DataNascimento = new DateTime(1970, 1, 1)
+            };
+
+            Aluno aluno5 = new Aluno()
+            {
+                Nome = "Ana",
+                DataNascimento = new DateTime(1990, 1, 1)
+            };
+
+            List<Aluno> alunos = new List<Aluno>();
+            alunos.Add(aluno1);
+            alunos.Add(aluno2);
+            alunos.Add(aluno3);
+            alunos.Add(aluno4);
+            alunos.Add(aluno5);
+
+            //para fazer a ordenação:
+            alunos.Sort();
+
+            foreach(var aluno in alunos)
+            {
+                Console.WriteLine(aluno);
+            }
         }
     }
 
-    class Aluno
+    class Aluno : IComparable
     {
         public string Nome { get; set; }
         public DateTime DataNascimento { get; set; }
@@ -51,6 +78,22 @@ namespace Programming._3.equals
         public override int GetHashCode()
         {
             return HashCode.Combine(Nome, DataNascimento);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null)
+                return 1;
+
+            Aluno outro = obj as Aluno;
+            if (outro == null)
+                throw new ArgumentException("não é um aluno");
+
+            var resultado = this.DataNascimento.CompareTo(outro.DataNascimento);
+            if (resultado == 0)
+                resultado = this.Nome.CompareTo(outro.Nome);
+
+            return resultado;
         }
     }
 }
