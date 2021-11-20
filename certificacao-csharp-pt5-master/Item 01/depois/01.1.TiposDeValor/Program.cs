7069d27e8fb7f337cc506baf68bd89c7aea6eedc
marinajulia <marinajulia707@gmail.com>
2021-11-20 19:45:20 -0300

    heap

---
+++ certificacao-csharp-pt5-master/Item 01/depois/01.1.TiposDeValor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _01._1.TiposDeValor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Metodo1();
+            Console.ReadKey();
+        }
+
+        static void Metodo1()
+        {
+            Metodo2(12);
+            Console.WriteLine("Saindo do método 1...");
+        }
+
+        static void Metodo2(int dados)
+        {
+            int multiplicador = 2;
+            Console.WriteLine("O valor é: "
+                + dados.ToString());
+            Metodo3(dados * multiplicador);
+        }
+
+        static void Metodo3(int dados)
+        {
+            Console.WriteLine("O dobro é: "
+                + dados.ToString());
+        }
+    }
+}
